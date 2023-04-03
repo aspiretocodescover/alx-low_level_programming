0b33a0fb73b04156e0c277ed0f66d0a3e3dfaf19
Aspiretocodescover <ritadorothy7@gmail.com>
2023-04-03 13:10:26 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _menset - this fills memory with a constant byte
+ * @a: pointer
+ * @b: constant bytes
+ * @c: First byte of memory area
+ */
+
+char *_menset(char *s, char b, unsigned int n)
+{
+        int i;
+
+        for (i = 0; n > 0; i++)
+        {
+                s(i) = b;
+                n--
+        }
+        return (s);
+}
