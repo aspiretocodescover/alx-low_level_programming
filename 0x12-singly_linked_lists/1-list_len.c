cab7c62463382b028fecc2baa97d6875fcda3df2
Aspiretocodescover <ritadorothy7@gmail.com>
2023-04-27 13:27:10 -0700

    listlen

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * list_len - a function that returns the number of elements in a
  * linked list_t list
  * @h: elements of type list_t
- * Return: number of elements
+ * Return: y
  */
 
 size_t list_len(const list_t *h);
 {
-	size_t i = 0;
+	size_t y = 0;
 
 	for (; h != NULL; h = h->next)
 	{
-		i++
+		y++
 	}
-		return (i);
+		return (y);
 }
