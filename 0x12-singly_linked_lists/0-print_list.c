8272ebc83dfd23014b7210dc6cce5e099365c762
Aspiretocodescover <ritadorothy7@gmail.com>
2023-04-27 13:07:43 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h);
 {
 	size_t i = 0;
 
-	for (; h != NULL; h = h->next; i++)
+	for (; h != NULL, h = h->next, i++)
 
 		if (h->str == NULL)
 		printf("[0] (nil)");
