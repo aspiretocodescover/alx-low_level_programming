0ee1f0f430610c829dfeea41a0c710742599b4df
Aspiretocodescover <ritadorothy7@gmail.com>
2023-05-02 15:28:13 -0700

    update task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0
+	size_t i = 0;
 		while (h != NULL)
 		{
 		printf("%d\n", h->n);
@@ -19,4 +19,3 @@ size_t print_listint(const listint_t *h)
 		}
 	return (i);
 }
-
